2cbd3d4dc4eca164e233d20dd45ed43daab2961e
mumeenakshi <mumeenakshi@MEENAKSHI>
2026-02-11 18:49:20 +0530

    code push for switch days

---
+++ my_maven/src/main/java/Switch/SampleSwitchDays.java
@@ -0,0 +1,25 @@
+package Switch;
+
+public class SampleSwitchDays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a= "orange";
+		switch(a)
+		{
+		case "orange":
+		System.out.println("orange");
+		break;
+		
+		case "apple":
+		System.out.println("apple");
+		break;
+		
+		default:
+		System.out.println("orange");	
+		}
+	
+	}
+
+}
